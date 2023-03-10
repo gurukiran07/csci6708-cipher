5b1ec5a68dcf2ea4e7edfb25ea07dad4c525a39e
Guru Jagata <gr421676@dal.ca>
2023-03-09 23:53:34 -0400

    PLAYFAIR: clang formatted

--- headers/playfair.h
+++ headers/playfair.h
@@ -1,33 +1,33 @@
 #ifndef PLAYFAIR_CIPHER_CSCI6708_H
 #define PLAYFAIR_CIPHER_CSCI6708_H
 
-#include <string>
 #include <array>
-#include <vector>
+#include <string>
 #include <utility>
+#include <vector>
 
-namespace crypt{
+namespace crypt {
 
-    const std::string ALPHABETS = "ABCDEFGHIKLMNOPQRSTUVWXYZ";
-    const std::string PUNCTUATION = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~\n";
-    const char REPEATED_CHAR_Q = 'Q';
-    const char REPEATED_CHAR_X = 'X';
-    const char ODD_LENGTH_STR = 'Z';
+const std::string ALPHABETS = "ABCDEFGHIKLMNOPQRSTUVWXYZ";
+const std::string PUNCTUATION = " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~\n";
+const char REPEATED_CHAR_Q = 'Q';
+const char REPEATED_CHAR_X = 'X';
+const char ODD_LENGTH_STR = 'Z';
 
-    inline bool is_removable_char(unsigned char chr){
-        return (PUNCTUATION.find(chr) != std::string::npos);
-    }
-
-    using idx_pair_t = std::array<std::pair<int, int>, 2>;
-    using digraph_t = std::vector<std::pair<char, char>>;
-    using matrix_t = std::array<std::array<char, 5>, 5>;
-
-    idx_pair_t idx_pair(std::pair<char, char>, matrix_t&);
-    matrix_t generate_matrix(std::string);
-    digraph_t make_digraphs(std::string);
-    std::string encrypt(std::string, matrix_t);
-    std::string decrypt(std::string, matrix_t);
-    void print(matrix_t, std::string, std::string);
+inline bool is_removable_char(unsigned char chr) {
+  return (PUNCTUATION.find(chr) != std::string::npos);
 }
+
+using idx_pair_t = std::array<std::pair<int, int>, 2>;
+using digraph_t = std::vector<std::pair<char, char>>;
+using matrix_t = std::array<std::array<char, 5>, 5>;
+
+idx_pair_t idx_pair(std::pair<char, char>, matrix_t &);
+matrix_t generate_matrix(std::string);
+digraph_t make_digraphs(std::string);
+std::string encrypt(std::string, matrix_t);
+std::string decrypt(std::string, matrix_t);
+void print(matrix_t, std::string, std::string);
+} // namespace crypt
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
+
 #include "playfair.h"
 
-int main(){
-    auto matrix = crypt::generate_matrix("MICROSOFT");
-    std::string cipher_text = crypt::encrypt("NEXT VERSION BEING RELEASED", matrix);
-    std::string plain_text = crypt::decrypt(cipher_text, matrix);
-    crypt::print(
-        matrix, "MICROSOFT", "NEXT VERSION BEING RELEASED"
-    );
+int main() {
+  auto matrix = crypt::generate_matrix("MICROSOFT");
+  std::string cipher_text =
+      crypt::encrypt("NEXT VERSION BEING RELEASED", matrix);
+  std::string plain_text = crypt::decrypt(cipher_text, matrix);
+  crypt::print(matrix, "MICROSOFT", "NEXT VERSION BEING RELEASED");
 }
